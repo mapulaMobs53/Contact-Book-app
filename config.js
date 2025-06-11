d9832d8cc0c4ec25b1591bf81fa473379044b2ae
mapulaMobs53 <masekwamengmapula5@gmail.com>
2025-06-11 08:19:22 +0200

    styling

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
-let rootpath = "https://mysite.itvarsity.org/api/ContactBook/";
-let apiKey = checkApiKey();
-
-function checkApiKey()
-{
-    if(!localStorage.getItem("apiKey"))
-    {
-        window.open("enter-api-key.html","_self");
-
-    }   
+function checkApiKey() {
+    if(!localStorage.getItem("apiKey")) {
+        window.open("enter-api-key.html", "_self");
+        return null; // Return null if no API key found
+    }
     return localStorage.getItem("apiKey");
 }
+
+let rootpath = "https://mysite.itvarsity.org/api/ContactBook/";
+let apiKey = checkApiKey();
